a4965ec0f0e437c18fb2deff78c10b87c5886f5c
F-Clement <>
2023-04-26 21:42:46 +0000

    set even listeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,23 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("class") === "login") {
+                alert("User Logged inn.");
+            } else if (this.getAttribute("class") === "submit-answer") {
+                alert("Answer Submitted!");
+            } else if (this.getAttribute("class") === "submit-review") {
+                alert("Review submitted.");
+            } else {
+                let tournament = this.getAttribute("data-type");
+                alert(`you selected ${tournament} tournament`);
+            }
+        });
+    }
+});
+
+
 function runGame() {
 
 }
